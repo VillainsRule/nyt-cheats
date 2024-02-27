900275f99e83e576c3ab19032443871cd6945dd2
plaguer <101288516+VillainsRule@users.noreply.github.com>
2024-02-27 08:35:42 -0500

    clean up wordle code
    no fixes, just cleanup to the fetch statement.

--- wordle/script.js
+++ wordle/script.js
@@ -1,4 +1,4 @@
-(() => {
+(async () => {
     if (!location.href.includes('nytimes.com/games/wordle')) return alert('Please visit the New York Times game Wordle to run this script.');
     if (!confirm('Are you sure you want to see today\'s answer? There is no going back!'));
 
@@ -8,5 +8,8 @@
         day: '2-digit',
         timeZone: 'America/New_York'
     }).replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2');
-    fetch(`/svc/wordle/v2/${date}.json`).then(r => r.json()).then(r => alert(`NYTIMES WORDLE | ANSWER\n    Answer: ${r.solution}\n\nenjoy :D`));
+
+    let answer = await  fetch(`/svc/wordle/v2/${date}.json`);
+    answer = await answer.json();
+    alert(`NYTIMES WORDLE | ANSWER\n    Answer: ${answer.solution}\n\nenjoy :D`);
 })();
