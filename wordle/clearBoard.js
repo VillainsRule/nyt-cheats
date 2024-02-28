2af976d3ef7fb36e659bfb1e852e4e5c4c03efb3
plaguer <101288516+VillainsRule@users.noreply.github.com>
2024-02-28 09:08:30 -0500

    forgot clearboard change

--- wordle/clearBoard.js
+++ wordle/clearBoard.js
@@ -4,6 +4,7 @@
     parsed.game.boardState = new Array(6).fill('');
     parsed.game.currentRowIndex = 0;
     parsed.game.status = 'IN_PROGRESS';
+    parsed.stats.hasPlayed = false;
 
     localStorage['nyt-wordle-moogle/ANON'] = JSON.stringify(parsed);
     location.reload();
