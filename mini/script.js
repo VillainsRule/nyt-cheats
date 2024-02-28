40101563d4c2a92facafe33b795ae0ed93898435
plaguer <101288516+VillainsRule@users.noreply.github.com>
2024-02-28 08:44:26 -0500

    improve variable names

--- mini/script.js
+++ mini/script.js
@@ -1,24 +1,24 @@
 (async () => {
     if (!location.href.includes('nytimes.com/crosswords/game/mini')) return alert('Please visit the New York Times crossword Mini to run this script.');
-    if (!confirm('Are you sure you want to see today\'s answers? There is no going back!'));
+    if (!confirm('Are you sure you want to see today\'s answer? There is no going back!'));
 
     let miniRequest = await fetch('/svc/crosswords/v6/puzzle/mini.json');
     let mini = await miniRequest.json();
 
     const chunks = Array.from({
         length: Math.sqrt(mini.body[0].cells.length)
-    }, (_) => []);
+    }, () => []);
 
-    mini.body[0].cells.forEach((value, index) => chunks[index%chunks.length].push(value));
+    mini.body[0].cells.forEach((answer, index) => chunks[index % chunks.length].push(answer));
 
     const board = [];
-    for (let i = 0; i < chunks[0].length; i++) {
+    for (let columns = 0; columns < chunks[0].length; columns++) {
         const innerArray = [];
-        for (let j = 0; j < chunks.length; j++) innerArray.push(chunks[j][i]);
+        for (let rows = 0; rows < chunks.length; rows++) innerArray.push(chunks[rows][columns]);
         board.push(innerArray);
     };
 
     let answers = 'NYTIMES CROSSWORD MINI | ANSWERS\n';
-    for (let i = 1; i <= board.length; i++) answers += `    Row ${i}: [ ${board[i - 1].map(a => a.answer || '◾').join(' ')} ]\n`;
+    for (let row = 1; row <= board.length; row++) answers += `    Row ${row}: [ ${board[row - 1].map(a => a.answer || '◾').join(' ')} ]\n`;
     alert(answers);
 })();
